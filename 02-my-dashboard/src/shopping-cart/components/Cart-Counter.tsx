dcbdca71042c3cdde6ee53c2ad3ebec7338799a1
Kevin Sarria <camilo.bravo.2050@gmail.com>
2025-05-28 00:26:49 -0500

    avances

--- 02-my-dashboard/src/shopping-cart/components/Cart-Counter.tsx
+++ 02-my-dashboard/src/shopping-cart/components/Cart-Counter.tsx
@@ -1,17 +1,38 @@
-'use client'
+"use client";
 
-import { useState } from "react";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import {
+  addOne,
+  initCounterState,
+  substractOne,
+} from "@/store/counter/counterSlice";
 
 interface Props {
-    value?: number
+  value?: number;
 }
 
-export const CartCounter = ({ value = 0 }: Props) => {
-  const [count, setCount] = useState<number>(value);
+export interface CounterResponse {
+  count: number;
+}
 
-  const handleCount = (operation: number) => {
-    setCount((prev) => prev + operation);
-  };
+const getApiCounter = async (): Promise<CounterResponse> => {
+  const data = await fetch("/api/counter").then((resp) => resp.json());
+
+  return data;
+};
+
+export const CartCounter = ({ value = 0 }: Props) => {
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
+  // useEffect(() => {
+  //   dispatch(initCounterState(value));
+  // }, [dispatch, value]);
+
+  useEffect(() => {
+    getApiCounter().then(({ count }) => dispatch(initCounterState(count)));
+  }, [dispatch]);
 
   return (
     <>
@@ -20,14 +41,14 @@ export const CartCounter = ({ value = 0 }: Props) => {
       <div className="flex">
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 cursor-pointer"
-          onClick={() => handleCount(+1)}
+          onClick={() => dispatch(addOne())}
         >
           +1
         </button>
 
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 cursor-pointer"
-          onClick={() => handleCount(-1)}
+          onClick={() => dispatch(substractOne())}
         >
           -1
         </button>
